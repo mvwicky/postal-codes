d64fa4ea95c53bd5831ac6563a97d7ebb7000428
mvwicky <mvanwickle@gmail.com>
2023-08-15 09:39:21 -0700

    Miscellaneous data fetching tweaks

--- src/data.ts
+++ src/data.ts
@@ -35,6 +35,7 @@ class DataLoader {
   readonly #params: CountryParams;
   readonly #log: ConsolaInstance;
   readonly #options: Required<LoadOptions>;
+  readonly #dataDir: string;
   readonly #file: string;
 
   constructor(
@@ -52,11 +53,8 @@ class DataLoader {
       timeout: config.defaultTimeout,
       ...options,
     };
-    this.#file = path.resolve(
-      Deno.cwd(),
-      config.dataDir,
-      params.outputFileName,
-    );
+    this.#dataDir = path.resolve(Deno.cwd(), config.dataDir);
+    this.#file = path.join(this.#dataDir, params.outputFileName);
   }
 
   static async create(
@@ -89,12 +87,15 @@ class DataLoader {
     this.#log.info(`Data file path: ${this.#file}`);
     const shouldFetch = await this.checkShouldFetch();
     if (!shouldFetch) {
+      await Deno.mkdir(this.#dataDir, { recursive: true });
       const buf = await this.fetch();
       if (buf) {
         const res = await this.extract(buf);
         if (!res) {
           return null;
         }
+        const now = new Date();
+        await Deno.utime(this.#file, now, now);
       } else {
         return null;
       }
@@ -106,7 +107,11 @@ class DataLoader {
     for await (const [key, entry] of this.parse(fd)) {
       data.set(key, entry);
     }
-    return data.size ? data : null;
+    const { size } = data;
+    if (size) {
+      cache.set(this.#name, data);
+    }
+    return size ? data : null;
   }
 
   private async checkShouldFetch(): Promise<boolean> {
@@ -165,6 +170,7 @@ class DataLoader {
           stream.on("end", () => {
             this.#log.info(`Stream ended`);
             resValue = this.#file;
+            zipFile.readEntry();
           });
           stream.pipe(outFile);
         } else {
@@ -206,142 +212,12 @@ class DataLoader {
   }
 }
 
-async function fetchCountryData(url: URL): Promise<ArrayBuffer | null> {
-  const res = await fetch(url);
-  if (res.ok && res.body) {
-    const arrBuf = await res.arrayBuffer();
-    return arrBuf;
-  }
-  return null;
-}
-
-async function extractCountryData(
-  buf: ArrayBuffer,
-  dataFileName: string,
-  dataFilePath: string,
-): Promise<string | null> {
-  const log = logger();
-  const zipFile = await zip.fromBuffer(buf, { lazyEntries: true });
-  const openReadStream = zip.makeOpenReadStream(zipFile);
-  return new Promise<string | null>((resolve, reject) => {
-    zipFile.on("entry", async (e) => {
-      const entry = e as zip.yauzl.Entry;
-      log.info(`Got entry: ${entry.fileName}`);
-      if (entry.fileName === dataFileName) {
-        log.info(`Entry matches data file name`);
-        const stream = await openReadStream(entry);
-        const outFile = createWriteStream(dataFilePath);
-        stream.on("end", () => {
-          log.info(`Stream ended`);
-          resolve(dataFilePath);
-        });
-        stream.pipe(outFile);
-      } else {
-        zipFile.readEntry();
-      }
-    });
-    zipFile.on("end", () => {
-      log.info("ZIP File Ended");
-    });
-    zipFile.on("error", (event) => reject(event));
-    zipFile.readEntry();
-  });
-}
-
-async function* parseCountryData(
-  dataFile: string,
-): AsyncGenerator<[string, GeoName]> {
-  const file = await Deno.open(dataFile, { read: true });
-  const rows = file.readable
-    .pipeThrough(new TextDecoderStream())
-    .pipeThrough(
-      new csv.CsvParseStream({
-        skipFirstRow: false,
-        separator: "\t",
-        columns: GEO_COLUMNS,
-      }),
-    );
-  let [total, failed] = [0, 0];
-  for await (const elem of rows) {
-    total += 1;
-    const res = GeoNameSchema.safeParse(elem);
-    if (res.success) {
-      const key = normKey(res.data.postal_code);
-      yield [key, res.data];
-    } else {
-      failed += 1;
-    }
-  }
-  logger().info(`${failed} failures out of ${total} total rows`);
-}
-
-async function getDataFile(params: CountryParams): Promise<string | null> {
-  const log = logger();
-  const cfg = await getConfig();
-  const dataDir = path.resolve(Deno.cwd(), cfg.dataDir);
-  // const dirExists = await exists(dataDir, { isDirectory: true });
-  await ensureDir(dataDir);
-  // if (!dirExists) {
-  //   log.info(`Creating data directory (${dataDir})`);
-  // }
-  const dataFilePath = path.join(dataDir, params.outputFileName);
-  log.info(`Data file path: ${dataFilePath}`);
-  const fileExists = await exists(dataFilePath, { isFile: true });
-  if (!fileExists) {
-    log.info(`Fetching file from ${params.url.href} (dest: ${dataFilePath})`);
-    const buf = await fetchCountryData(params.url);
-    if (buf) {
-      return extractCountryData(buf, params.dataFileName, dataFilePath);
-    }
-  } else {
-    log.info(`File already exists.`);
-  }
-  return dataFilePath;
-}
-
-async function* streamCountryData(
-  params: CountryParams,
-): AsyncGenerator<[string, GeoName]> {
-  const dataFilePath = await getDataFile(params);
-  if (dataFilePath) {
-    yield* parseCountryData(dataFilePath);
-  }
-}
-
-async function doLoadCountryData(
-  params: CountryParams,
-): Promise<CountryData | null> {
-  const data: CountryData = new Map();
-  for await (const [key, entry] of streamCountryData(params)) {
-    data.set(key, entry);
-  }
-  return data.size ? data : null;
-}
-
 async function loadCountryData(
   country: string,
-  { forceReload }: Partial<LoadOptions> = {},
+  options: Partial<LoadOptions> = {},
 ): Promise<CountryData | null> {
-  const cNorm = await checkCountry(country);
-  if (cNorm) {
-    if (forceReload) {
-      defaultCache.delete(cNorm);
-    }
-    const cachedData = defaultCache.get(cNorm);
-    if (cachedData) {
-      return cachedData;
-    }
-    const params = COUNTRIES.get(cNorm);
-    if (params) {
-      const data = await doLoadCountryData(params);
-      if (data) {
-        defaultCache.set(cNorm, data);
-      }
-      return data;
-    }
-  }
-
-  return null;
+  const loader = await DataLoader.create(country, options);
+  return loader?.load() ?? null;
 }
 
 export { DataLoader, loadCountryData };
