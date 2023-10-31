c012f09dac1a16d92ea4fedb367ed161dd271596
Michael Van Wickle <mvanwickle@gmail.com>
2023-10-31 14:01:31 -0700

    Switch from oak to hono (#2)
    * Initial hono commit
    
    * Add distance route tests

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
-import { bFormat, oak, Status } from "../deps.ts";
+import { bFormat, cors, hono, Status } from "../deps.ts";
 import { loadCountryData } from "./data.ts";
 import { hDist } from "./distance.ts";
 import { logger } from "./log.ts";
 import { normKey, toPoint } from "./utils.ts";
-
-type CountryParams = { country: string };
-type InfoParams = CountryParams & { code: string };
-type DistParams = CountryParams & { start: string; end: string };
 
 function logMemory(start: Deno.MemoryUsage, end: Deno.MemoryUsage) {
   const log = logger();
@@ -22,90 +18,77 @@ const countryErrorString = (countryParam: string) =>
 
 const codeErrorString = (codeParam: string) => `Invalid code: ${codeParam}`;
 
-const router = new oak.Router();
-router.get<InfoParams>(
-  "/info/:code/",
-  async ({ params: { country, code }, ...ctx }) => {
-    const countryData = await loadCountryData(country);
-    const codeInfo = countryData?.get(normKey(code));
-    if (codeInfo) {
-      ctx.response.body = codeInfo;
-    } else if (!countryData) {
-      ctx.response.status = Status.BadRequest;
-      ctx.response.body = { error: [countryErrorString(country)] };
-    } else {
-      ctx.response.status = Status.BadRequest;
-      ctx.response.body = { error: [codeErrorString(code)] };
-    }
-  },
-).get<DistParams>("/distance/:start/:end/", async ({ params, ...ctx }) => {
-  const countryData = await loadCountryData(params.country);
-  const startCode = normKey(params.start);
-  const endCode = normKey(params.end);
+const countryApp = new hono.Hono().basePath(":country/");
+countryApp.get("info/:code/", async (c) => {
+  const { code, country } = c.req.param();
+  const countryData = await loadCountryData(country);
+  const codeInfo = countryData?.get(normKey(code));
+  if (codeInfo) {
+    return c.json(codeInfo);
+  } else if (!countryData) {
+    c.status(Status.BadRequest);
+    return c.json({ error: [countryErrorString(country)] });
+  } else {
+    c.status(Status.BadRequest);
+    return c.json({ error: [codeErrorString(code)] });
+  }
+}).get("distance/:start/:end/", async (c) => {
+  const { country, start, end } = c.req.param();
+  const countryData = await loadCountryData(country);
+  const startCode = normKey(start);
+  const endCode = normKey(end);
   const startInfo = countryData?.get(startCode);
   const endInfo = countryData?.get(endCode);
   if (startInfo && endInfo) {
     const distance = hDist(toPoint(startInfo), toPoint(endInfo));
-    ctx.response.body = { startInfo, endInfo, distance };
+    return c.json({ start: startInfo, end: endInfo, distance });
   } else {
     const error: string[] = [];
     if (!countryData) {
-      error.push(countryErrorString(params.country));
+      error.push(countryErrorString(country));
     }
     if (countryData && !startInfo) {
-      error.push(codeErrorString(params.start));
+      error.push(codeErrorString(start));
     }
     if (countryData && !endInfo) {
-      error.push(codeErrorString(params.end));
+      error.push(codeErrorString(end));
     }
-    ctx.response.status = Status.BadRequest;
-    ctx.response.body = { error };
+    c.status(Status.BadRequest);
+    return c.json({ error });
   }
-}).get<CountryParams>("/random/", async (ctx) => {
-  const countryData = await loadCountryData(ctx.params.country);
+}).get("random/", async (c) => {
+  const { country } = c.req.param();
+  const countryData = await loadCountryData(country);
   if (countryData) {
     const allCodes = [...countryData.keys()];
     const code = allCodes[Math.floor(Math.random() * allCodes.length)];
     const codeInfo = countryData.get(code)!;
-    ctx.response.body = codeInfo;
+    return c.json(codeInfo);
   } else {
-    ctx.response.status = Status.BadRequest;
-    ctx.response.body = { error: [countryErrorString(ctx.params.country)] };
+    c.status(Status.BadRequest);
+    return c.json({ error: [countryErrorString(country)] });
   }
 });
 
-const r = new oak.Router().use(
-  "/api/:country",
-  router.routes(),
-  router.allowedMethods(),
-).get("/health/", (ctx) => {
-  ctx.response.body = { ready: true };
-});
-
-const app = new oak.Application();
-app.use(async (ctx, next) => {
-  logger().info(`${ctx.request.method} ${ctx.request.url}`);
-  await next();
-}).use(async (_ctx, next) => {
-  const startUsage = Deno.memoryUsage();
-  await next();
-  const endMem = Deno.memoryUsage();
-  queueMicrotask(() => logMemory(startUsage, endMem));
-}).use(async (ctx, next) => {
-  const start = performance.now();
-  await next();
-  const elapsed = performance.now() - start;
-  ctx.response.headers.set("Server-Timing", `cpu;dur=${elapsed.toFixed(3)}`);
-}).use(async ({ response: { headers } }, next) => {
-  await next();
-  headers.append("Access-Control-Allow-Origin", "*");
-}).use(r.routes(), r.allowedMethods());
-
-app.addEventListener("listen", ({ hostname, port, secure }) => {
-  const scheme = `http${secure ? "s" : ""}`;
-  logger().info(
-    `Listening on ${scheme}://${hostname ?? "localhost"}:${port}`,
-  );
-});
+const app = new hono.Hono();
+app
+  .use("*", async (ctx, next) => {
+    const start = performance.now();
+    await next();
+    const elapsed = performance.now() - start;
+    ctx.header("Server-Timing", `cpu;dur=${elapsed.toFixed(3)}`);
+  })
+  .use("*", cors())
+  .use("*", async ({ req }, next) => {
+    logger().info(`${req.method} ${req.url}`);
+    await next();
+  }).use("*", async (_ctx, next) => {
+    const startUsage = Deno.memoryUsage();
+    await next();
+    const endMem = Deno.memoryUsage();
+    queueMicrotask(() => logMemory(startUsage, endMem));
+  });
+app.route("/api/", countryApp);
+app.get("/health/", (c) => c.json({ ready: true }));
 
 export { app };
